5f9db74416ee5b92231a40c17773966270483280
Sebastian Schock <sebschock@gmail.com>
2017-02-16 12:34:23 +0100

    Correct tests

--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -5,7 +5,7 @@ def test_create_from_google_play_receipt():
     receipt = {
         'orderId': 1337,
         'productId': 'pew pew',
-        'purchaseTime': '01.01.2016 12:00'
+        'purchaseTimeMillis': '1486132449000'
     }
     purchase = Purchase.from_google_play_receipt(receipt)
 
@@ -19,7 +19,7 @@ def test_create_from_test_google_play_receipt():
     receipt = {
         'purchaseToken': 1337,
         'productId': 'pew pew',
-        'purchaseTime': '01.01.2016 12:00'
+        'purchaseTimeMillis': '1486132449000'
     }
     purchase = Purchase.from_google_play_receipt(receipt)
 
@@ -33,7 +33,7 @@ def test_create_from_app_store_receipt():
     receipt = {
         'transaction_id': 1337,
         'product_id': 'pew pew',
-        'purchase_date': '01.01.2016 12:00',
+        'purchase_date_ms': '1486132449000',
         'quantity': 100500
     }
     purchase = Purchase.from_app_store_receipt(receipt)
